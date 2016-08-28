dd2692b0433f39ae18399047765b4efc47b0a83b
Troy Denkinger <tdenkinger@gmail.com>
2016-08-28 10:16:29 -0400

    Create tests for the sequence server.

--- sequence/test/sequence_test.exs
+++ sequence/test/sequence_test.exs
@@ -2,7 +2,23 @@ defmodule SequenceTest do
   use ExUnit.Case
   doctest Sequence
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "Sequence server" do
+    setup do
+      :sys.replace_state(Sequence.Server, fn {_old_state, pid} -> {0, pid} end)
+      :ok
+    end
+
+    test "returns the first number when called" do
+      {start_value, _} =  :sys.get_state Sequence.Server
+      new_value = Sequence.Server.next_number
+      assert new_value == start_value
+    end
+
+    test "increments the number on subsequent calls" do
+      {start_value, _} =  :sys.get_state Sequence.Server
+      _ = Sequence.Server.next_number
+      new_value = Sequence.Server.next_number
+      assert new_value == start_value + 1
+    end
   end
 end
