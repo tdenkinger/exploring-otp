a4141de56af23111ea9a85257c9fbd6cdcf1b836
Troy Denkinger <tdenkinger@gmail.com>
2016-10-12 08:14:07 -0400

    Start a command parsing module and refactor the server.

--- elixir-lang/kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ elixir-lang/kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -1,14 +1,53 @@
 defmodule KVServer do
   use Application
 
+  require Logger
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      # worker(KVServer.Worker, [arg1, arg2, arg3]),
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [4040]])
     ]
 
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port,
+                                    [:binary,
+                                     packet: :line,
+                                     active: false,
+                                     reuseaddr: true])
+
+    Logger.info "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
+  end
+
 end
